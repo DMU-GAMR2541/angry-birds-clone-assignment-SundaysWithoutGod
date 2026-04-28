58a7c6639ccf4b1a5ec992ccd9ed55d8340be060
SundaysWithoutGod <sianmpmmstclair@gmail.com>
2026-04-28 17:11:40 +0100

    Adding in Box2D
    Setting up the Box2D physics world

--- include/DynamicObject.h
+++ include/DynamicObject.h
@@ -13,6 +13,15 @@ private:
 	//Create sprite
 	//Load texture
 	//Load File--use if/else for checking
+
+	//Box2D Dynamic 
+	b2Vec2 b2d_Pos;//Position-
+	b2BodyDef b2d_bodyDef;//Have a object body find our what's its doing or define what it is
+	b2FixtureDef b2d_fixtureDef;//Values that are attached to the object body like density
+	b2Body* b2d_Body;//The actuall body object
+	b2CircleShape b2d_dynamicShape;//the shell around the image that would be the 'collider'
+
+	
 	
 public:
 	
@@ -28,8 +37,12 @@ public:
 
 	//passing a in a string that will be the location in the file  of this sprite instance
 	DynamicObject(std::string location, sf::Vector2f PosIn, sf::Vector2f scale);
-	
+
+	//Box2d constructor
+	DynamicObject(b2World ObjectsWorld, std::string location, sf::Vector2f PosIn, sf::Vector2f scale);
+
 	void  setSprite(std::string location);
+
 
 	sf::Sprite getSprite() {
 
--- src/DynamicObject.cpp
+++ src/DynamicObject.cpp
@@ -20,6 +20,33 @@ DynamicObject::DynamicObject(std::string location, sf::Vector2f PosIn, sf::Vecto
 
 
 
+DynamicObject::DynamicObject(b2World ObjectsWorld, std::string location, sf::Vector2f PosIn, sf::Vector2f scale)
+{
+	if (!spriteTexture.loadFromFile(location)) {
+		std::cout << "Texture NOT loadiing" << std::endl;
+
+	}
+	else
+	{
+		spriteRender.setTexture(spriteTexture);
+		//setting the origins-taking it from the corner to the center point
+		spriteRender.setOrigin(spriteRender.getLocalBounds().getSize().x / 2.0f, spriteRender.getLocalBounds().getSize().y / 2.0f);
+	}
+
+	spriteRender.setScale(scale);
+	spriteRender.setPosition(sf::Vector2f(PosIn.x, PosIn.y));
+
+
+
+	//Box2D
+	b2d_bodyDef.type = b2_dynamicBody;
+	b2d_bodyDef.position = b2d_Pos;
+	//Creates the bodies in the world
+	b2d_Body = ObjectsWorld.CreateBody(&b2d_bodyDef);
+
+
+}
+
 void DynamicObject::setSprite(std::string location)
 {
 	if (!spriteTexture.loadFromFile(location)) {
